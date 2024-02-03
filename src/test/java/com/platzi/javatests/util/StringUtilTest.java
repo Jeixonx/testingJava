cb9b6d441ccb3bdddf68b59df2ce3d34be83033d
Jeison Triana <triana.jeison@gmail.com>
2024-02-03 12:16:22 -0500

    clase 2: lanzar una excepcion para alertar sobre un error

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -1,21 +1,13 @@
 package com.platzi.javatests.util;
-
+import static org.junit.Assert.assertEquals;
 
 public class StringUtilTest {
 
     public static void main(String[] args) {
 
-        String result = StringUtil.repeat("hola", 3);
-        System.out.println(result);
-        if(!result.equals("holaholahola")){
-            System.out.println("ERROR");
-        }
-
-        String result2 = StringUtil.repeat("hola", 1);
-        System.out.println(result2);
-        if(!result2.equals("hola")){
-            System.out.println("ERROR");
-        }
+        assertEquals( "holaholahola", StringUtil.repeat("hola", 2));
+        assertEquals( "holaholahola", StringUtil.repeat("hola", 1));
+        
     }
   
 }
